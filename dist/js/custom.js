1218656d1c06810a2cb1f6ea97b03f1b79e28c33
hanifff24 <shidqihanif12@gmail.com>
2025-01-25 23:03:14 +0700

    Add files via upload

---
+++ dist/js/custom.js
@@ -0,0 +1,7 @@
+function ubahTema() {
+    const root = document.documentElement; 
+    root.style.setProperty('--primary', '#FF5733'); 
+    root.style.setProperty('--secondary', '#C70039');
+    root.style.setProperty('--light', '#FFC300');
+    root.style.setProperty('--dark', '#900C3F');
+}
